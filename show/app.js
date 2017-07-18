c445334ca4e719d3bbbc246c57a311e34fbdf295
Danilo Rodrigues <danilo@danilo.ws>
2017-07-17 21:35:32 -0300

    upload

---
+++ show/app.js
@@ -0,0 +1,52 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    });
+
+
+
+  var code = getParameterByName('code');
+  
+var token = code,
+    num_photos = 13;
+ 
+$.ajax({
+	url: 'https://api.instagram.com/v1/users/self/media/recent',
+	dataType: 'jsonp',
+	type: 'GET',
+	data: {access_token: token, count: num_photos},
+	success: function(data){
+		for( x in data.data ){
+			if(x>=0) {
+				$('.carousel-inner').append('<div class="item active"><ul class="thumbnails"><li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			}
+			else if(x==7) {
+				$('.carousel-inner').append('</ul></div><div class="item"><ul class="thumbnails"><li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			}
+			else if(x==14) {
+				$('.carousel-inner').append('</ul></div><div class="item"><ul class="thumbnails"><li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			}
+			else if(x==data.data.length) {
+				$('.carousel-inner').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li></ul></div>');
+			}
+			else {
+				$('.carousel-inner').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			}
+			
+		}
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
+
+  });
