abe57c11a8f2b0eefb31eba738f29b88412a178b
Danilo Rodrigues <danilo@danilo.ws>
2017-07-19 16:26:54 -0300

    sdsdsd

--- show/app.js
+++ show/app.js
@@ -25,26 +25,23 @@ $.ajax({
 	type: 'GET',
 	data: {access_token: token, count: num_photos},
 	success: function(data){
+		console.log(data);
 		for( x in data.data){
 			if(x==0) {
 				console.log(x+' é 0');
-				$('.carousel-inner').append('<div class="item active"><ul class="thumbnails"></ul></div>');
+				$('.carousel-inner').append('<div class="item one active"><ul class="thumbnails"></ul></div>');
 			}
 			if(x==7) {
 				console.log(x+' é 7');
-				$('.carousel-inner').append('<div class="item"><ul class="thumbnails"></ul></div>');			
+				$('.carousel-inner').append('<div class="item two"><ul class="thumbnails"></ul></div>');			
 			}
-			if(x==14) {
-				console.log(x+' é 14');
-				$('.carousel-inner').append('<div class="item"><ul class="thumbnails"></ul></div>');
-			}			
 			if(x<7) {
 				console.log(x+' é menor 7');
-				$('.item:nth-child(0) ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+				$('.item one) ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
 			if(x>7 && x<14) {
 				console.log(x+' e maior que 7 e menor 14');
-				$('.item:nth-child(1) ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+				$('.item two ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
 		}
 	},
