ea1fd3f94384c7283b26a0483cf1e569aa8450c5
Danilo Rodrigues <danilo@danilo.ws>
2017-07-20 15:12:12 -0300

    fdfddf

--- show/app.js
+++ show/app.js
@@ -96,20 +96,20 @@ $.ajax({
 			if(x==0) {
 				$('.carousel-inner').append('<div class="item one active"><ul class="thumbnails"></ul></div>');
 			}
-			if(x==6) {
+			if(x==5) {
 				$('.carousel-inner').append('<div class="item two"><ul class="thumbnails"></ul></div>');			
 			}
-			if(x==11) {
+			if(x==10) {
 				$('.carousel-inner').append('<div class="item tree"><ul class="thumbnails"></ul></div>');			
 			}
-			if(x<6){
-				$('.item.one ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x<5){
+				$('.item.one ul.thumbnails').append('<li class="col-sm-2-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
-			if(x>=5 && x<12) {
-				$('.item.two ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x>=5 && x<10) {
+				$('.item.two ul.thumbnails').append('<li class="col-sm-2-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
-			if(x>=12 && x<17) {
-				$('.item.tree ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x>=10 && x<15) {
+				$('.item.tree ul.thumbnails').append('<li class="col-sm-2-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
 		}
 	},
@@ -129,20 +129,20 @@ $.ajax({
 			if(x==0) {
 				$('.carousel-inner').append('<div class="item one active"><ul class="thumbnails"></ul></div>');
 			}
-			if(x==7) {
+			if(x==6) {
 				$('.carousel-inner').append('<div class="item two"><ul class="thumbnails"></ul></div>');			
 			}
-			if(x==13) {
+			if(x==12) {
 				$('.carousel-inner').append('<div class="item tree"><ul class="thumbnails"></ul></div>');			
 			}
-			if(x<7){
-				$('.item.one ul.thumbnails').append('<li class="col-sm-1"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x<6){
+				$('.item.one ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
-			if(x>=7 && x<13) {
-				$('.item.two ul.thumbnails').append('<li class="col-sm-1"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x>=6 && x<12) {
+				$('.item.two ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
-			if(x>=13 && x<19) {
-				$('.item.tree ul.thumbnails').append('<li class="col-sm-1"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
+			if(x>=12 && x<17) {
+				$('.item.tree ul.thumbnails').append('<li class="col-sm-2"><div class="fff"><div class="thumbnail"><a href="'+data.data[x].link+'" target="_blank"><img alt="" src="'+data.data[x].images.low_resolution.url+'"></a></div></div></li>');
 			}
 		}
 	},
