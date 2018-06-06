7862b7552280c358a8652f22076f89af7d576a9f
Danilo Rodrigues <danilo@danilo.ws>
2018-06-06 12:53:16 -0300

    new
    new

---
+++ new/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/selcukcura/pen/GEJvrw.
+
+ Instafeed and Owl Carousel 2 example
---
+++ new/js/index-vs.js
@@ -0,0 +1,81 @@
+// How to get an access token:
+// http://jelled.com/instagram/access-token
+
+// {{model.user.username}}, {{likes}} likes
+
+var galleryFeed = new Instafeed({
+  get: "user",
+  userId: 5412091892,
+  accessToken: "5412091892.b8b0534.ee1ec71bb8324574ba67bc26625855be",
+  resolution: "standard_resolution",
+  useHttp: "true",
+  limit: 10,
+  template: 
+    // '<a href="{{image}}">'+
+      '<div class="img-featured-container">'+
+        '<div class="img-backdrop"></div>'+
+        '<div class="description-container">'+
+          // '<p class="caption">{{caption}}</p>'+
+          '<span class="likes"><i class="icon ion-heart"></i> {{likes}}</span>'+
+          '<span class="comments"><i class="icon ion-chatbubble"></i> {{comments}}</span>'+
+        '</div>'+
+        '<img src="{{image}}" class="img-responsive">'+
+      '</div>',
+    // '</a>'+
+  target: "instafeed-gallery-feed",
+  after: function() {
+    // disable button if no more results to load
+    if (!this.hasNext()) {
+      btnInstafeedLoad.setAttribute('disabled', 'disabled');
+    }
+    
+    var owl = $(".owl-carousel"),
+        owlSlideSpeed = 300;
+
+    // init owl    
+    $(document).ready(function(){
+      owl.owlCarousel({
+        // navContainer: '.owl-nav-custom',
+        // dotsContainer: '.owl-dots-custom',
+        margin:10,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:6000,
+        autoplayHoverPause:true,
+        nav:true,
+        responsive:{
+          0:{
+            items:1
+          },
+          200:{
+            items:2
+          },
+          400:{
+            items:3
+          },
+          768:{
+            items:5
+          }
+        }
+      });
+    });
+
+    // keyboard controls
+    $(document.documentElement).keydown(function(event) {
+      if (event.keyCode == 37) {
+        owl.trigger('prev.owl.carousel', [owlSlideSpeed]);
+      }
+      else if (event.keyCode == 39) {
+        owl.trigger('next.owl.carousel', [owlSlideSpeed]);
+      }
+    });
+  }
+});
+
+galleryFeed.run();
+
+var btnInstafeedLoad = document.getElementById("btn-instafeed-load");
+btnInstafeedLoad.addEventListener("click", function() {
+  galleryFeed.next()
+});
